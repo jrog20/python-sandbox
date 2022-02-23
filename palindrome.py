f41c85c4f1714dc9530cbcd1c4e54707388256a7
Jaime Rogstad <jaimerogstad@gmail.com>
2022-02-23 12:56:22 -0500

    Fix function

--- palindrome.py
+++ palindrome.py
@@ -1,12 +1,29 @@
+# Method:
+
+# Declare a variable with the length of the string.
+# Declare a for loop, using half of the length of the string as a reference point.
+# Check if each letter is the same as its mirror equivalent — or, 
+# a character on the other side (measured with index — 1).
+
 def palindrome(string):
-    if string[0] != string[-1]:
-        return False
+    l = len(string)
+    for i in range(l//2):
+        if string[i] != string[l-i-1]:
+            return False
     return True
 
 string = 'carerac'
 print('Expect: True')
 print(palindrome(string))
 
-string = 'hello'
+string = 'helloh'
 print('Expect: False')
+print(palindrome(string))
+
+string = 'jfdkjlfds'
+print('Expect: False')
+print(palindrome(string))
+
+string = 'scarpepracs'
+print('Expect: True')
 print(palindrome(string))
