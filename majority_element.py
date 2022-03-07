f792cc3658753552938c5e90ce3f7de9b8246564
Jaime Rogstad <jaimerogstad@gmail.com>
2022-03-07 18:00:03 -0500

    Add sort function

--- majority_element.py
+++ majority_element.py
@@ -15,11 +15,11 @@
 
 #########################################################################################
 # Brute force
-# Time complexity : O(n^2)
+# Time complexity: O(n^2)
 # The brute force algorithm contains two nested for loops that each run for nn iterations, 
 # adding up to quadratic time complexity.
 
-# Space complexity : O(1)
+# Space complexity: O(1)
 # The brute force solution does not allocate additional space proportional to the input size.
 
 # def majority_element(nums):
@@ -31,17 +31,27 @@
 
 #########################################################################################
 # Hashmap solution
-# Time complexity : O(n)
-# Space complexity : O(n)
+# Time complexity: O(n)
+# Space complexity: O(n)
 
-import collections
-from collections import Counter
+# import collections
+# from collections import Counter
 
-def majority_element(nums):
-    counts = collections.Counter(nums)
-    return max(counts.keys(), key=counts.get)
+# def majority_element(nums):
+#     counts = collections.Counter(nums)
+#     return max(counts.keys(), key=counts.get)
 
 #########################################################################################
+# Sort solution
+# Time complexity: O(n log n)
+# Space complexity: O(n)
+
+def majority_element(nums):
+    nums.sort()
+    return nums[len(nums)//2]
+
+#########################################################################################
+
 nums = [3,2,3]
 print('Expecting: 3')
 print('Output:', majority_element(nums))
