59b3b71640f2305560d52ee611ea1af7eaeecc8e
Jaime Rogstad <jaimerogstad@gmail.com>
2022-03-27 20:16:21 -0400

    Add function

--- array/product_except_self.py
+++ array/product_except_self.py
@@ -16,8 +16,44 @@
 # Output: [0,0,9,0,0]
 
 ################################################################################################
+# Since we cannot use the division operation, we can't just multiply all the elements together 
+# and divide by nums[i]. We can, however, get the product of all the nums to the left of nums[i]
+# and the product of all the nums to the right of nums[i], and multiply those together.
+################################################################################################
 def product_except_self(nums):
-    answer = 0
+    # The length of the input array 
+    length = len(nums)
+    
+    # The left and right arrays
+    left, right, answer = [0]*length, [0]*length, [0]*length
+    
+    # left[i] contains the product of all the elements to the left
+    # For the element at index '0', there are no elements to the left,
+    # so the left[0] would be 1
+    left[0] = 1
+    for i in range(1, length):
+        # left[i - 1] already contains the product of elements to the left of 'i - 1'
+        # Multiplying it with nums[i - 1] would give the product of all elements 
+        # to the left of index 'i'
+        left[i] = nums[i - 1] * left[i - 1]
+    
+    # right[i] contains the product of all the elements to the right
+    # For the element at index 'length - 1', there are no elements to the right, so the 
+    # right[length - 1] would be 1
+    right[length - 1] = 1
+    for i in reversed(range(length - 1)):
+        # right[i + 1] already contains the product of elements to the right of 'i + 1'
+        # Multiplying it with nums[i + 1] would give the product of all elements 
+        # to the right of index 'i'
+        right[i] = nums[i + 1] * right[i + 1]
+    
+    # Constructing the answer array
+    for i in range(length):
+        # For the first element, right[i] would be product except self
+        # For the last element of the array, product except self would be left[i]
+        # Else, multiple product of all elements to the left and to the right
+        answer[i] = left[i] * right[i]
+    
     return answer
 ################################################################################################
 
