569ff19062dc333aab7ab789769ee3a1bd339b1a
Jaime Rogstad <jaimerogstad@gmail.com>
2022-04-23 15:35:51 -0400

    Fix iteration length of for loop

--- ale_test/find_runs_final.py
+++ ale_test/find_runs_final.py
@@ -22,7 +22,9 @@
 
 def solution(values, run_length):
     solution_array = []
-    for i in range(len(values)-run_length):
+    # Need to add +1 to range in order to get any potential runs at the end of array
+    # i.e. I originally stopped the loop one index short
+    for i in range(len(values)-run_length+1):
         counter = 0
         increase_array = []
         decrease_array = []
@@ -44,9 +46,40 @@ def solution(values, run_length):
                 counter += 1
                 temp_index += 1
     return solution_array
-        ##########################################################################
+##########################################################################
 
 values = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 7]
 run_length = 3 
 print('Expecting: [0, 4, 6, 7]')
+print('This is the solution:', solution(values, run_length))
+
+# Original submitted code didn't work for a run starting at the last possible index
+values = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 9]
+run_length = 3 
+print('Expecting: [0, 4, 6, 7, 10]')
+print('This is the solution:', solution(values, run_length))
+
+values = [1, 7, 3, 5, 10, 2]
+run_length = 3 
+print('Expecting: []')
+print('This is the solution:', solution(values, run_length))
+
+values = [1, 2, 3, 5, 10, 9, 8, 9, 10, 11, 7, 8, 9]
+run_length = 4
+print('Expecting: [6]')
+print('This is the solution:', solution(values, run_length))
+
+values = [1, 2, 3, 4]
+run_length = 5
+print('Expecting: []')
+print('This is the solution:', solution(values, run_length))
+
+values = [1, 2, 3, 4, 5]
+run_length = 5
+print('Expecting: [0]')
+print('This is the solution:', solution(values, run_length))
+
+values = [-1, -2, -3, -4, -5]
+run_length = 5
+print('Expecting: [0]')
 print('This is the solution:', solution(values, run_length))
